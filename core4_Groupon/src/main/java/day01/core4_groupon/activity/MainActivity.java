9d8cfd1c0abafc3488d2d83002c9baecd232eae9
tarena <693370949@QQ.COM>
2017-06-16 10:44:06 +0800

    A

--- core4_Groupon/src/main/java/day01/core4_groupon/activity/MainActivity.java
+++ core4_Groupon/src/main/java/day01/core4_groupon/activity/MainActivity.java
@@ -1,15 +1,76 @@
 package day01.core4_groupon.activity;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshListView;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import day01.core4_groupon.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_PullToRefreshListView)
+    PullToRefreshListView pullToRefreshListView;
+
+    ListView listView;
+    ArrayList<String> datas;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initListView();
+    }
+
+    private void initListView() {
+        listView = pullToRefreshListView.getRefreshableView();
+        datas = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,datas);
+        listView.setAdapter(adapter);
+
+        //添加下拉松手后的刷新
+        pullToRefreshListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onRefresh(PullToRefreshBase<ListView> refreshView) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        datas.add(0,"新增内容");
+                        adapter.notifyDataSetChanged();
+                        pullToRefreshListView.onRefreshComplete();
+                    }
+                },2000);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        refresh();
+    }
+
+    private void refresh() {
+        datas.add("aaa");
+        datas.add("bbb");
+        datas.add("ccc");
+        datas.add("ddd");
+        datas.add("eee");
+        datas.add("fff");
+        datas.add("ggg");
+        datas.add("hhh");
+        datas.add("iii");
+        datas.add("jjj");
     }
 }
