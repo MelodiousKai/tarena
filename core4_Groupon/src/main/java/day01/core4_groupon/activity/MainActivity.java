f7ad8fb16ac40348fcb63e1db165da0942daa878
tarena <693370949@QQ.COM>
2017-06-16 16:37:53 +0800

    A

--- core4_Groupon/src/main/java/day01/core4_groupon/activity/MainActivity.java
+++ core4_Groupon/src/main/java/day01/core4_groupon/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package day01.core4_groupon.activity;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
@@ -13,12 +20,35 @@ import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import day01.core4_groupon.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    //头部
+    @BindView(R.id.main_top_LinearLayout)
+    LinearLayout city_LinearLayout;//城市按钮
+
+    @BindView(R.id.main_top_location_TextView)
+    TextView city_TextView;//城市文字
+
+    @BindView(R.id.main_top_seek_TextView)
+    TextView seek_TextView; //搜索按钮
+
+    @BindView(R.id.main_top_more_ImageButton)
+    ImageButton menu_ImageButton;//菜单按钮
+
+    @BindView(R.id.main_menu)
+    LinearLayout menu_LinearLayout;//菜单按钮
+
+
+    //中段
     @BindView(R.id.main_PullToRefreshListView)
     PullToRefreshListView pullToRefreshListView;
+
+    //底部
+    @BindView(R.id.main_bottom_RadioGroup)
+    RadioGroup radioGroup;
 
     ListView listView;
     ArrayList<String> datas;
@@ -53,6 +83,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //为ListView添加若干头部
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View list_ads=inflater.inflate(R.layout.header_list_ads,listView,false);
+        View list_categories=inflater.inflate(R.layout.header_list_categories,listView,false);
+        View list_icons=inflater.inflate(R.layout.header_list_icons,listView,false);
+        View list_recommend=inflater.inflate(R.layout.header_list_recommend,listView,false);
+        View list_square=inflater.inflate(R.layout.header_list_square,listView,false);
+
+        listView.addHeaderView(list_icons);//增加listview的头部
+        listView.addHeaderView(list_square);
+        listView.addHeaderView(list_ads);
+        listView.addHeaderView(list_categories);
+        listView.addHeaderView(list_recommend);
+
+
     }
 
     @Override
@@ -73,4 +118,20 @@ public class MainActivity extends AppCompatActivity {
         datas.add("iii");
         datas.add("jjj");
     }
+
+    @OnClick(R.id.main_top_LinearLayout)
+    public void jumpToCity(View view){
+        //Intent intent = new Intent(this,CityActivity.class);
+        //startActivity(intent);
+    }
+
+    @OnClick(R.id.main_top_more_ImageButton)
+    public void setMenu(View view){
+        if (menu_LinearLayout.getVisibility() == View.VISIBLE){
+            menu_LinearLayout.setVisibility(View.INVISIBLE);
+        }{
+            menu_LinearLayout.setVisibility(View.VISIBLE);
+        }
+    }
+
 }
